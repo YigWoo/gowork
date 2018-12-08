103fcfb6f4b019d67a72157cbb90baf7152502c8
吴佚超 <wuyichao@lattebank.com>
2018-12-08 14:24:55 +0800

    reapply whole dir

---
+++ mapreduce/common_reduce.go
@@ -0,0 +1,106 @@
+package mapreduce
+
+import (
+	"encoding/json"
+	"os"
+	"log"
+)
+
+type KeyValues map[string][]string
+
+// doReduce manages one reduce task: it reads the intermediate
+// key/value pairs (produced by the map phase) for this task, sorts the
+// intermediate key/value pairs by key, calls the user-defined reduce function
+// (reduceF) for each key, and writes the output to disk.
+func doReduce(
+	jobName string,       // the name of the whole MapReduce job
+	reduceTaskNumber int, // which reduce task this is
+	outFile string,       // write the output here
+	nMap int,             // the number of map tasks that were run ("M" in the paper)
+	reduceF func(key string, values []string) string,
+) {
+	//
+	// You will need to write this function.
+	//
+	// You'll need to read one intermediate file from each map task;
+	// reduceName(jobName, m, reduceTaskNumber) yields the file
+	// name from map task m.
+	//
+	// Your doMap() encoded the key/value pairs in the intermediate
+	// files, so you will need to decode them. If you used JSON, you can
+	// read and decode by creating a decoder and repeatedly calling
+	// .Decode(&kv) on it until it returns an error.
+	//
+	// You may find the first example in the golang sort package
+	// documentation useful.
+	//
+	// reduceF() is the application's reduce function. You should
+	// call it once per distinct key, with a slice of all the values
+	// for that key. reduceF() returns the reduced value for that key.
+	//
+	// You should write the reduce output as JSON encoded KeyValue
+	// objects to the file named outFile. We require you to use JSON
+	// because that is what the merger than combines the output
+	// from all the reduce tasks expects. There is nothing special about
+	// JSON -- it is just the marshalling format we chose to use. Your
+	// output code will look something like this:
+	//
+	// enc := json.NewEncoder(file)
+	// for key := ... {
+	// 	enc.Encode(KeyValue{key, reduceF(...)})
+	// }
+	// file.Close()
+	//
+	var keyValues = KeyValues{}
+
+	for mapTaskNumber := 0; mapTaskNumber < nMap; mapTaskNumber++ {
+		mapFileName := reduceName(jobName, mapTaskNumber, reduceTaskNumber)
+
+		file, err := os.Open(mapFileName)
+
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		decoder := json.NewDecoder(file)
+
+		var kv KeyValue = KeyValue{}
+
+		for decoder.More() {
+			err = decoder.Decode(&kv)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if keyValues[kv.Key] == nil {
+				values := []string{}
+				values = append(values, kv.Value)
+				keyValues[kv.Key] = values
+			} else {
+				values := keyValues[kv.Key]
+				values = append(values, kv.Value)
+				keyValues[kv.Key] = values
+			}
+		}
+	}
+
+	out, err := os.Create(outFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	encoder := json.NewEncoder(out)
+
+	for key, values := range keyValues {
+		reduceResult := reduceF(key, values)
+		kv := KeyValue{key, reduceResult}
+		err = encoder.Encode(kv)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+	out.Close()
+}
---
+++ mapreduce/schedule.go
@@ -0,0 +1,103 @@
+package mapreduce
+
+import (
+	"fmt"
+	"sync"
+)
+
+//
+// schedule() starts and waits for all tasks in the given phase (Map
+// or Reduce). the mapFiles argument holds the names of the files that
+// are the inputs to the map phase, one per map task. nReduce is the
+// number of reduce tasks. the registerChan argument yields a stream
+// of registered workers; each item is the worker's RPC address,
+// suitable for passing to call(). registerChan will yield all
+// existing registered workers (if any) and new ones as they register.
+//
+func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
+	var ntasks int
+	var n_other int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mapFiles)
+		n_other = nReduce
+	case reducePhase:
+		ntasks = nReduce
+		n_other = len(mapFiles)
+	}
+
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+
+	// All ntasks tasks have to be scheduled on workers, and only once all of
+	// them have been completed successfully should the function return.
+	// Remember that workers may fail, and that any given worker may finish
+	// multiple tasks.
+
+	switch phase {
+	case mapPhase:
+		var wg sync.WaitGroup
+		for taskNumber, file := range mapFiles {
+			wg.Add(1)
+			go func(file string, n_other int, taskNumber int) {
+				defer wg.Done()
+				workerUrl := <-registerChan
+
+				doTaskArgs := DoTaskArgs{
+					File:          file,
+					JobName:       jobName,
+					Phase:         mapPhase,
+					NumOtherPhase: n_other,
+					TaskNumber:    taskNumber,
+				}
+
+				isOk := false
+				for !isOk {
+					isOk = call(workerUrl, "Worker.DoTask", doTaskArgs, nil)
+					if isOk {
+						/* worker 完成工作后，将 workerUrl 放回 channel 中，可以给下一个 */
+						go func() {
+							registerChan <- workerUrl
+						}()
+					} else {
+						workerUrl = <- registerChan
+					}
+				}
+
+			}(file, n_other, taskNumber)
+		}
+		wg.Wait()
+
+	case reducePhase:
+		var wg sync.WaitGroup
+		for i := 0; i < nReduce; i++ {
+			wg.Add(1)
+			go func(taskNumber int) {
+				defer wg.Done()
+				workerUrl := <-registerChan
+				doTaskArgs := DoTaskArgs{
+					File:          "",
+					JobName:       jobName,
+					Phase:         reducePhase,
+					NumOtherPhase: n_other,
+					TaskNumber:    taskNumber,
+				}
+
+				isOk := false
+				for isOk != true {
+					isOk = call(workerUrl, "Worker.DoTask", doTaskArgs, nil)
+					if isOk {
+						go func() {
+							registerChan <- workerUrl
+						}()
+					} else {
+						workerUrl = <- registerChan
+						fmt.Printf("%v, %v", isOk, workerUrl)
+					}
+				}
+			}(i)
+		}
+		wg.Wait()
+	}
+
+	fmt.Printf("Schedule: %v phase done\n", phase)
+}
